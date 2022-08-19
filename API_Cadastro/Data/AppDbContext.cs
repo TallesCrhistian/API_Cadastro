496b4e27865f30ebcd8b64252bdee8f7e6c10d40
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-08-19 10:55:40 -0300

    feat: Adicionando ConnectionString

--- API_Cadastro/Data/AppDbContext.cs
+++ API_Cadastro/Data/AppDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace API_Cadastro.Data
+﻿using API_Cadastro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Cadastro.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+      
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+       
     }
 }
--- API_Cadastro/Models/Pessoa.cs
+++ API_Cadastro/Models/Pessoa.cs
@@ -2,5 +2,10 @@
 {
     public class Pessoa
     {
+        public int Id { get; set; }
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
     }
 }
