7a1096f3d7ecd1b09674e46bba96aaeaea747fcd
tallesChristian <72113346+tallesChristian@users.noreply.github.com>
2022-09-01 21:12:02 -0300

    Initial

--- API_Cadastro/Migrations/20220902000156_Initial.Designer.cs
+++ API_Cadastro/Migrations/20220902000156_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API_Cadastro.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20220823164323_Config de Pessoa")]
-    partial class ConfigdePessoa
+    [Migration("20220902000156_Initial")]
+    partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
--- API_Cadastro/Migrations/20220902000156_Initial.cs
+++ API_Cadastro/Migrations/20220902000156_Initial.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace API_Cadastro.Migrations
 {
-    public partial class Root : Migration
+    public partial class Initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -14,10 +15,11 @@ namespace API_Cadastro.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                    cpf = table.Column<string>(type: "varchar(14)", nullable: false),
+                    nome = table.Column<string>(type: "varchar(45)", nullable: false),
+                    email = table.Column<string>(type: "varchar(40)", nullable: false),
+                    telefone = table.Column<string>(type: "varchar(15)", nullable: false),
+                    last_update = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "getdate()")
                 },
                 constraints: table =>
                 {
